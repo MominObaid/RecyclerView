9be99d5fd65b8b5b269c8e77d005949f3ee04018
obaid <obaidurrahmanobaid@gmail.com>
2023-05-10 10:37:48 +0530

    Revert "RV code runs without image"
    This reverts commit f2eeb5c2ad6201f443dfc3528f8cc89bf3dddd7a.

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.recyclerview
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,33 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-
+        val itemList = listOf<String>("Alex","Sam","John","Tony")
         val MainList = findViewById<RecyclerView>(R.id.listView)
-        MainList.adapter = MyAdapter(itemList)
+        MainList.adapter = MyAdapter(this, itemList)
         MainList.layoutManager = LinearLayoutManager(this)
 
     }
-
-    val itemList = listOf<itemListt>(
-        itemListt(
-            "Tom", "New Message"
-        ),
-        itemListt ("Sam", "Student"),
-        itemListt("Tim", "Student", ),
-        itemListt("Alex", "Employee"),
-        itemListt("abc", "xyz"),
-        itemListt("def", "zyx"),
-        itemListt("ghi", "age"),
-        itemListt("jkl", "bbbbb"),
-        itemListt("bbbbb", "ccccc"),
-    itemListt("ccccc","zzzzzzzz")
-    )
-
-    data class itemListt(
-        var Name: String,
-        var Detail: String,
-//        var Image: ImageView
-    )
-
 }
-
--- app/src/main/java/com/example/recyclerview/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerview/MyAdapter.kt
@@ -1,13 +1,13 @@
 package com.example.recyclerview
 
-import android.graphics.Color
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_list2.view.*
 
-class MyAdapter(val list: List<MainActivity.itemListt>):
+class MyAdapter (val context: Context, val list: List<String>):
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder{
@@ -17,24 +17,16 @@ class MyAdapter(val list: List<MainActivity.itemListt>):
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int){
-        holder.textName.text = list[position].Name
-        holder.textMessage.text = list[position].Detail
-        holder.imageView.imageView2
-
-
-//        var color = "#FFFFFF"
-//        if (position % 2 == 0) { color = "#CCCCCC"}
-//            holder.LayoutColor.setBackgroundColor(Color.parseColor(color))
+        holder.textName.text = list[position]
+        holder.imgView.text = list[position]
     }
 
-    override fun getItemCount(): Int {
+    override fun getItemCount(): Int{
         return list.size
     }
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-        val textName = itemView.txtName
-        val textMessage = itemView.Message
-        val imageView = itemView.imageView2
-//        val LayoutColor = itemView.LayoutId
+    class MyViewHolder(view: View) {
+        val textName = view.txtName
+        val imgView = view.Message
     }
 }
